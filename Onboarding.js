32631e05be4906a22d8a650ff6a73172e2526167
Felipe Rojas <feli.rojasr@duocuc.cl>
2025-11-08 13:30:07 -0300

    List of questions.

---
+++ Onboarding.js
@@ -0,0 +1,171 @@
+const groups = [
+    { words: [
+        //1
+        { text: "Entusiasta", type: "I"},
+        { text: "Rápido(a)", type: "D"},
+        { text: "Lógico(a)", type: "C"},
+        { text: "Apacible", type: "S"},
+
+        //2
+        { text: "Cauteloso(a)", type: "C"},
+        { text: "Decidido(a)", type: "D"},
+        { text: "Receptivo(a)", type: "I"},
+        { text: "Bondadoso(a)", type: "S"},
+        
+        //3
+        { text: "Amigable", type: "I"},
+        { text: "Preciso(a)", type: "C"},
+        { text: "Franco(a)", type: "D"},
+        { text: "Tranquilo(a)", type: "S"},
+
+        //4
+        { text: "Elocuente", type: "I"},
+        { text: "Controlado(a)", type: "C"},
+        { text: "Tolerante", type: "S"},
+        { text: "Decisivo(a)", type: "D"},
+
+        //5
+        { text: "Atrevido(a)", type: "D"},
+        { text: "Concienzudo(a)", type: "C"},
+        { text: "Comunicativo(a)", type: "I"},
+        { text: "Moderado(a)", type: "S"},
+
+        //6
+        { text: "Ameno(a)", type: "S"},
+        { text: "Ingenioso(a)", type: "I"},
+        { text: "Investigador(a)", type: "C"},
+        { text: "Acepta Riesgos", type: "D"},
+
+        //7
+        { text: "Expresivo", type: "I"},
+        { text: "Cuidadoso(a)", type: "C"},
+        { text: "Dominante", type: "D"},
+        { text: "Sensible", type: "S"},
+
+        //8
+        { text: "Extrovertido(a)", type: "I"},
+        { text: "Precavido(a)", type: "C"},
+        { text: "Constante", type: "S"},
+        { text: "Impaciente", type: "D"},
+
+        //9
+        { text: "Discreto(a)", type: "C"},
+        { text: "Complaciente", type: "S"},
+        { text: "Encantador(a)", type: "I"},
+        { text: "Insistente", type: "D"},
+
+        //10
+        { text: "Valeroso(a)", type: "D"},
+        { text: "Anima a los demás", type: "I"},
+        { text: "Pacífico(a)", type: "S"},
+        { text: "Perfeccionista", type: "C"},
+
+        //11
+        { text: "Reservado(a)", type: "C"},
+        { text: "Atento(a)", type: "S"},
+        { text: "Osado(a)", type: "D"},
+        { text: "Alegre", type: "I"},
+
+        //12
+        { text: "Estimulante", type: "I"},
+        { text: "Gentil", type: "S"},
+        { text: "Perceptivo(a)", type: "C"},
+        { text: "Independiente", type: "D"},
+
+        //13
+        { text: "Competitivo(a)", type: "D"},
+        { text: "Considerado(a)", type: "S"},
+        { text: "Alegre", type: "I"},
+        { text: "Sagaz", type: "C"},
+
+        //14
+        { text: "Meticuloso(a)", type: "C"},
+        { text: "Obediente", type: "S"},
+        { text: "Ideas Firmes", type: "D"},
+        { text: "Alentador(a)", type: "I"},
+
+        //15
+        { text: "Popular", type: "I"},
+        { text: "Reflexivo(a)", type: "C"},
+        { text: "Tenaz", type: "D"},
+        { text: "Calmado(a)", type: "S"},
+
+        //16
+        { text: "Analitico(a)", type: "C"},
+        { text: "Audaz", type: "D"},
+        { text: "Leal", type: "S"},
+        { text: "Promotor(a)", type: "I"},
+
+        //17
+        { text: "Sociable", type: "I"},
+        { text: "Paciente", type: "S"},
+        { text: "Autosuficiente", type: "D"},
+        { text: "Certero(a)", type: "C"},
+
+        //18
+        { text: "Adaptable", type: "S"},
+        { text: "Resuelto(a)", type: "D"},
+        { text: "Prevenido(a)", type: "C"},
+        { text: "Vivaz", type: "I"},
+
+        //19
+        { text: "Agresivo(a)", type: "D"},
+        { text: "Impetuoso(a)", type: "I"},
+        { text: "Amistoso(a)", type: "S"},
+        { text: "Discerniente", type: "C"}, 
+
+        //20
+        { text: "De Trato Fácil", type: "I"},
+        { text: "Compasivo(a)", type: "S"},
+        { text: "Cauto(a)", type: "C"},
+        { text: "Habla Directo", type: "D"},
+
+        //21
+        { text: "Evaluador(a)", type: "C"},
+        { text: "Generoso(a)", type: "S"},
+        { text: "Animado(a)", type: "I"},
+        { text: "Persistente", type: "D"},
+
+        //22
+        { text: "Impulsivo(a)", type: "I"},
+        { text: "Cuida los detalles", type: "C"},
+        { text: "Enérgico(a)", type: "D"},
+        { text: "Tranquilo(a)", type: "S"},
+
+        //23
+        { text: "Sociable", type: "I"},
+        { text: "Sistemático(a)", type: "C"},
+        { text: "Vigoroso(a)", type: "D"},
+        { text: "Tolerante", type: "S"},
+
+        //24
+        { text: "Cautivador(a)", type: "I"},
+        { text: "Contento(a)", type: "S"},
+        { text: "Exigente", type: "D"},
+        { text: "Apegado(a) a las normas", type: "C"},
+
+        //25
+        { text: "Le agrada discutir", type: "D"},
+        { text: "Metódico(a)", type: "C"},
+        { text: "Comedido(a)", type: "S"},
+        { text: "Desenvuelto(a)", type: "I"},
+
+        //26
+        { text: "Jovial", type: "I"},
+        { text: "Preciso(a)", type: "C"},
+        { text: "Directo(a)", type: "D"},
+        { text: "Ecuánime", type: "S"},
+
+        //27
+        { text: "Inquieto(a)", type: "D"},
+        { text: "Amable", type: "S"},
+        { text: "Elocuente", type: "I"},
+        { text: "Cuidadoso(a)", type: "C"},
+
+        //28
+        { text: "Prudente", type: "C"},
+        { text: "Pionero(a)", type: "D"},
+        { text: "Espontáneo(a)", type: "I"},
+        { text: "Colaborador", type: "S"},
+    ]}
+];
